ee9a54b1df8c665142b099fb6cc15d54eb0c5abe
Kostya Sklyar <sklyar.kostya17@gmail.com>
2025-10-01 23:39:11 +0500

    Refactor locators and create test Operation Convert Points

--- RiderProjects/SeleniumTests/BankTests/BankUITests.cs
+++ RiderProjects/SeleniumTests/BankTests/BankUITests.cs
@@ -28,35 +28,84 @@ public class BankUITests
         driver.Navigate().GoToUrl("https://retail-tst.payment.ru/login");
         
         // Ввести логин и пароль клиента, нажать кнопку войти
-        driver.FindElement(Locators.loginInputLocator).SendKeys("131349");
-        driver.FindElement(Locators.passwordInputLocator).SendKeys("1111111");
-        wait.Until(ExpectedConditions.ElementExists(Locators.loginButtonLocator));
-        driver.FindElement(Locators.loginButtonLocator).Click();
+        driver.FindElement(Locators.Login.loginInputLocator).SendKeys("131349");
+        driver.FindElement(Locators.Login.passwordInputLocator).SendKeys("1111111");
+        wait.Until(ExpectedConditions.ElementExists(Locators.Login.loginButtonLocator));
+        driver.FindElement(Locators.Login.loginButtonLocator).Click();
         
         // Ввести проверочный код
-        driver.FindElement(Locators.confirmInputLocator).SendKeys("111111");
-        /*
-        // Раскрыть раздел "Бонусы", нажать на бонуcный счет
-        //driver.FindElement(Locators.sectionBonusesLocators).Click();
-        driver.FindElement(Locators.bonusAccountLocators).Click();
-        
-        //Нажать на кнопку "Потратить баллы"
-        driver.FindElement(Locators.spendPointsButtonLocator).Click();
-        */
+        driver.FindElement(Locators.Login.confirmInputLocator).SendKeys("111111");
         
         //Нажать на кнопку "Платежи и переводы" и проверить заголовок "Платежи и переводы"
-        driver.FindElement(Locators.paymentsAndTranferButtonLocator).Click();
-        Assert.That(driver.FindElement(Locators.paymentsAndTranfersTitleLocator).Text, Is.EqualTo("Платежи и переводы"));
+        driver.FindElement(Locators.Account.paymentsAndTranferButtonLocator).Click();
+        Assert.That(driver.FindElement(Locators.Account.paymentsAndTranfersTitleLocator).Text, Is.EqualTo("Платежи и переводы"));
         
         //Нажать на операцию перевода по номеру телефона, ввести номер телефона, нажать на кнопку с введенным телефоном
-        driver.FindElement(Locators.transferByPhoneNumberButtonLocator).Click();
-        driver.FindElement(Locators.phoneNumberInputLocator).SendKeys("9154561123");
-        driver.FindElement(Locators.submitPhoneNumberButtonLocator).Click();
-        Assert.That(driver.FindElement(Locators.systemTransferTitleLocator).Text, Is.EqualTo("Выбор системы перевода"));
+        driver.FindElement(Locators.Account.transferByPhoneNumberButtonLocator).Click();
+        driver.FindElement(Locators.Account.phoneNumberInputLocator).SendKeys("9154561123");
+        driver.FindElement(Locators.Account.submitPhoneNumberButtonLocator).Click();
+        Assert.That(driver.FindElement(Locators.Account.systemTransferTitleLocator).Text, Is.EqualTo("Выбор системы перевода"));
         
         //
-        driver.FindElement(Locators.choiceBankPSB).Click();
-        Assert.That(driver.FindElement(Locators.byPhoneNumberInPSBTitleLocator).Text, Is.EqualTo("По номеру телефона в ПСБ"));
+        driver.FindElement(Locators.Account.choiceBankPSB).Click();
+        Assert.That(driver.FindElement(Locators.Account.byPhoneNumberInPSBTitleLocator).Text, Is.EqualTo("По номеру телефона в ПСБ"));
+        
+        Thread.Sleep(TimeSpan.FromSeconds(5));
+    }
+    
+    [Test]
+    public void OperationConvertPointInRubles()
+    {
+        using var driver = CreateDriver();
+        var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+        driver.Navigate().GoToUrl("https://retail-tst.payment.ru/login");
+        
+        // Ввести логин и пароль клиента, нажать кнопку войти
+        driver.FindElement(Locators.Login.loginInputLocator).SendKeys("131349");
+        driver.FindElement(Locators.Login.passwordInputLocator).SendKeys("1111111");
+        wait.Until(ExpectedConditions.ElementExists(Locators.Login.loginButtonLocator));
+        driver.FindElement(Locators.Login.loginButtonLocator).Click();
+        
+        // Ввести проверочный код
+        driver.FindElement(Locators.Login.confirmInputLocator).SendKeys("111111");
+       
+        // Раскрыть раздел "Бонусы", нажать на бонуcный счет
+        driver.FindElement(Locators.ConvertRubles.sectionBonusesLocators).Click();
+        driver.FindElement(Locators.ConvertRubles.bonusAccountLocators).Click();
+        
+        // Нажать на кнопку "Потратить баллы"; проверить, что карточка называется "Конвертация"; на витрине нажать кнопку "Перевести в рубли"
+        driver.FindElement(Locators.ConvertRubles.spendPointsButtonLocator).Click();
+        
+        // Проверить, что карточка называется "Конвертация"; курс конвертации 1 балл = 1 Р; текст карточки корректный
+        Assert.That(driver.FindElement(Locators.ConvertRubles.cardConvertTitleLocator).Text, Is.EqualTo("Конвертация"));
+        Assert.That(driver.FindElement(Locators.ConvertRubles.conversionRateTitleLocator).Text, Is.EqualTo("1 балл = 1 ₽"));
+        Assert.That(driver.FindElement(Locators.ConvertRubles.infoConvertTextLocator).Text, Is.EqualTo("Можно перевести баллы в рубли на карту или на счёт"));
+        Assert.That(driver.FindElement(Locators.ConvertRubles.convertInRublesButtonLocator).Text, Is.EqualTo("Перевести в рубли"));
+        
+        // Нажать на кнопку "Перевести в рубли"
+        driver.FindElement(Locators.ConvertRubles.convertInRublesButtonLocator).Click();
+        
+        // Дождаться появления заголовка операции; проверить заголовок, код операции
+        wait.Until(ExpectedConditions.ElementExists(Locators.ConvertRubles.operationConvertBonusPointsTitleLocator));
+        Assert.That(driver.FindElement(Locators.ConvertRubles.operationConvertBonusPointsTitleLocator).Text, Is.EqualTo("Перевод Бонусных баллов в рубли на счёт карты"));
+        Assert.That(driver.FindElement(Locators.ConvertRubles.operationCodeConvertBonusPointsTitleLocator).Text, Is.EqualTo("код 612"));
+        
+        // Нажать на поле выбора карты, выбрать карту
+        Thread.Sleep(TimeSpan.FromSeconds(2));
+        driver.FindElement(Locators.ConvertRubles.paymentCardSelectionFieldLocator).Click();
+        string numberCard = driver.FindElement(Locators.ConvertRubles.numberCardLocator).Text;
+        Thread.Sleep(TimeSpan.FromSeconds(2));
+        driver.FindElement(Locators.ConvertRubles.choiceCardButtonLocator).Click();
+        
+        // Стереть сумму из второго поля, ввести 1
+        Assert.That(driver.FindElement(Locators.ConvertRubles.infoSumTextLocator).Text, Is.EqualTo("сумма в диапазоне от 500.00 до"));
+        driver.FindElement(Locators.ConvertRubles.sumPointsInputLocator).Clear();
+        driver.FindElement(Locators.ConvertRubles.sumPointsInputLocator).SendKeys("500");
+        
+        //Нажать кнопку "Продолжить"
+        driver.FindElement(Locators.ConvertRubles.continueButtonLocator).Click();
+        
+        Assert.That(driver.FindElement(Locators.ConvertRubles.selectedNumberCardLocator).Text, Is.EqualTo(numberCard));
         
         Thread.Sleep(TimeSpan.FromSeconds(5));
     }
--- RiderProjects/SeleniumTests/BankTests/LocatorsBank.cs
+++ RiderProjects/SeleniumTests/BankTests/LocatorsBank.cs
@@ -3,24 +3,42 @@ using OpenQA.Selenium;
 namespace SeleniumTests.LocatorsBank;
 public static class Locators
 {
-    public static By loginInputLocator => By.CssSelector("[name='username']");
-    public static By passwordInputLocator => By.CssSelector("[name='password']");
-    public static By loginButtonLocator => By.CssSelector("[type='submit']");
-    public static By confirmInputLocator => By.CssSelector("#confirmStepSms");
-    
-    /*public static By sectionBonusesLocators => By.CssSelector("#expansion-panel-19");
-    public static By bonusAccountLocators => By.XPath("//span[text()='Бонусный счёт']");
-    public static By spendPointsButtonLocator => By.CssSelector("[appearance='primary']");
-    public static By opeanAccountButtonLocator => By.XPath("//span[text(), 'Открыть счёт']");
-    */
-    
-    public static By paymentsAndTranferButtonLocator => By.CssSelector(".desktop-menu__list li:nth-child(2)");
-    public static By paymentsAndTranfersTitleLocator => By.CssSelector(".psb-title_type-title_3");
-    public static By transferByPhoneNumberButtonLocator => By.XPath("//psb-text[text()='По номеру телефона']");
-    public static By phoneNumberInputLocator => By.CssSelector("[type='text']");
-    public static By submitPhoneNumberButtonLocator => By.CssSelector(".operations-item__title");
-    public static By systemTransferTitleLocator => By.CssSelector(".psb-title_type-title_2");
-    public static By choiceBankPSB => By.XPath("//psb-text[text()='ПСБ']");
-    public static By byPhoneNumberInPSBTitleLocator => By.CssSelector(".operation-header__operation-name");
-
+    public class Login
+    {
+        public static By loginInputLocator => By.CssSelector("[name='username']");
+        public static By passwordInputLocator => By.CssSelector("[name='password']");
+        public static By loginButtonLocator => By.CssSelector("[type='submit']");
+        public static By confirmInputLocator => By.CssSelector("#confirmStepSms");
+    }
+    public class Account
+    {
+        public static By opeanAccountButtonLocator => By.XPath("//span[text(), 'Открыть счёт']");
+        public static By paymentsAndTranferButtonLocator => By.CssSelector(".desktop-menu__list li:nth-child(2)");
+        public static By paymentsAndTranfersTitleLocator => By.CssSelector(".psb-title_type-title_3");
+        public static By transferByPhoneNumberButtonLocator => By.XPath("//psb-text[text()='По номеру телефона']");
+        public static By phoneNumberInputLocator => By.CssSelector("[type='text']");
+        public static By submitPhoneNumberButtonLocator => By.CssSelector(".operations-item__title");
+        public static By systemTransferTitleLocator => By.CssSelector(".psb-title_type-title_2");
+        public static By choiceBankPSB => By.XPath("//psb-text[text()='ПСБ']");
+        public static By byPhoneNumberInPSBTitleLocator => By.CssSelector(".operation-header__operation-name");
+    }
+    public class ConvertRubles
+    {
+        public static By sectionBonusesLocators => By.CssSelector("rtl-sidebar-product-block:nth-child(8)");
+        public static By bonusAccountLocators => By.XPath("//span[text()='Бонусный счёт']");
+        public static By spendPointsButtonLocator => By.CssSelector("[appearance='primary']");
+        public static By cardConvertTitleLocator => By.CssSelector("rtl-account-showcase-card:last-child .psb-title");
+        public static By conversionRateTitleLocator => By.CssSelector("rtl-account-showcase-card:last-child .psb-status");
+        public static By infoConvertTextLocator => By.CssSelector("rtl-account-showcase-card:last-child .psb-text");
+        public static By convertInRublesButtonLocator => By.CssSelector("rtl-account-showcase-card:last-child .button");
+        public static By operationConvertBonusPointsTitleLocator => By.CssSelector(".operation-header__operation-name");
+        public static By operationCodeConvertBonusPointsTitleLocator => By.CssSelector(".operation-header__operation-code");
+        public static By paymentCardSelectionFieldLocator => By.CssSelector(".money-source-form");
+        public static By choiceCardButtonLocator => By.CssSelector("mat-option:first-child");
+        public static By numberCardLocator => By.CssSelector("mat-option:first-child .money-source-select-option__info-account");
+        public static By sumPointsInputLocator => By.CssSelector(".form-input-element");
+        public static By infoSumTextLocator => By.CssSelector(".invalid-feedback-message__text");
+        public static By continueButtonLocator => By.XPath("//span[contains(text(),'Продолжить')]");
+        public static By selectedNumberCardLocator => By.XPath("//div[contains(@class,'operation-confirm-param')][div[contains(text(),'Номер карты')]]/div[@class='operation-confirm-param__value']");
+    }
 }
